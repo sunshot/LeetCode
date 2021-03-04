cbb79cf0f8892d997d2f697b71801ea526395bee
Changhua <sunchanghua@gmail.com>
2021-03-04 10:17:47 +0800

    Add test case for ListNode

--- 19. Remove Nth Node From End of List/solution1.py
+++ 19. Remove Nth Node From End of List/solution1.py
@@ -1,8 +1,9 @@
+from typing import List
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         if not head:
@@ -30,4 +31,31 @@ class Solution:
             else:
                 print('wrong parameters')
                 return None
-      
+
+def convert2ListNode(nums: List[int]) -> ListNode:
+    dummy = ListNode(0)
+    curr = dummy
+    for x in nums:
+        node = ListNode(x)
+        curr.next = node
+        curr = curr.next
+    return dummy.next
+
+def convertFromListNode(head: ListNode) -> List[int]:
+    if not head:
+        return []
+    nums = []
+    while head:
+        nums.append(head.val)
+        head = head.next
+    return nums
+
+if __name__== '__main__':
+    solution = Solution()
+
+    nums = [1,2,3,4,5]
+    n = 2
+    head = convert2ListNode(nums)
+    head = solution.removeNthFromEnd(head, n)
+    result = convertFromListNode(head)
+    print(result)
--- 19. Remove Nth Node From End of List/solution2.py
+++ 19. Remove Nth Node From End of List/solution2.py
@@ -1,8 +1,9 @@
+from typing import List
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         if not head:
@@ -23,3 +24,30 @@ class Solution:
             slow.next = None
         return dummy.next
         
+def convert2ListNode(nums: List[int]) -> ListNode:
+    dummy = ListNode(0)
+    curr = dummy
+    for x in nums:
+        node = ListNode(x)
+        curr.next = node
+        curr = curr.next
+    return dummy.next
+
+def convertFromListNode(head: ListNode) -> List[int]:
+    if not head:
+        return []
+    nums = []
+    while head:
+        nums.append(head.val)
+        head = head.next
+    return nums
+
+if __name__== '__main__':
+    solution = Solution()
+
+    nums = [1,2,3,4,5]
+    n = 2
+    head = convert2ListNode(nums)
+    head = solution.removeNthFromEnd(head, n)
+    result = convertFromListNode(head)
+    print(result)
