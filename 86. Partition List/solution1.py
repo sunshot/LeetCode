1553acfec1a722b64fcf3d553e5be6263e870f45
Changhua <sunchanghua@gmail.com>
2021-03-04 09:52:02 +0800

    Add test case for ListNode

--- 86. Partition List/solution1.py
+++ 86. Partition List/solution1.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 from typing import List
 class Solution:
     def partition(self, head: ListNode, x: int) -> ListNode:
@@ -33,3 +33,31 @@ class Solution:
             else:
                 curr = curr.next
         return dummy.next
+
+def convert2ListNode(nums: List[int]) -> ListNode:
+    dummy = ListNode(0)
+    curr = dummy
+    for x in nums:
+        node = ListNode(x)
+        curr.next = node
+        curr = curr.next
+    return dummy.next
+
+def convertFromListNode(head: ListNode) -> List[int]:
+    if not head:
+        return []
+    nums = []
+    while head:
+        nums.append(head.val)
+        head = head.next
+    return nums
+
+if __name__== '__main__':
+    solution = Solution()
+
+    nums = [1,4,3,2,5,2]
+    x = 3
+    head = convert2ListNode(nums)
+    head = solution.partition(head, x)
+    result = convertFromListNode(head)
+    print(result)
--- 86. Partition List/solution2.py
+++ 86. Partition List/solution2.py
@@ -1,7 +1,7 @@
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 from typing import List
 class Solution:
     def partition(self, head: ListNode, x: int) -> ListNode:
@@ -22,3 +22,31 @@ class Solution:
         before.next = after_dummy.next
         head = before_dummy.next
         return head
+
+def convert2ListNode(nums: List[int]) -> ListNode:
+    dummy = ListNode(0)
+    curr = dummy
+    for x in nums:
+        node = ListNode(x)
+        curr.next = node
+        curr = curr.next
+    return dummy.next
+
+def convertFromListNode(head: ListNode) -> List[int]:
+    if not head:
+        return []
+    nums = []
+    while head:
+        nums.append(head.val)
+        head = head.next
+    return nums
+
+if __name__== '__main__':
+    solution = Solution()
+
+    nums = [1,4,3,2,5,2]
+    x = 3
+    head = convert2ListNode(nums)
+    head = solution.partition(head, x)
+    result = convertFromListNode(head)
+    print(result)
