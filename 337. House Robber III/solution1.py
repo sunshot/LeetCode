22b4afa7cee73dc6a45c8d60eccdfb331ab06543
Changhua <sunchanghua@gmail.com>
2021-03-15 23:39:41 +0800

    Add TreeNode test case

--- 337. House Robber III/solution1.py
+++ 337. House Robber III/solution1.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+from typing import List
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -22,3 +23,39 @@ class Solution:
         max_with_node, max_without_node = self.rob_tree(root)
         return max(max_with_node, max_without_node)
         
+
+def convert2TreeNode(nums: List[int]) -> TreeNode:
+    nodes = []
+    for num in nums:
+        if str(num) == 'null' or num == None:
+            nodes.append(None)
+        else:
+            node = TreeNode(num)
+            nodes.append(node)
+    if not nodes or len(nodes) == 0:
+        return None
+    curr = 0
+    level = 0
+    next_child = 2 ** level
+    while next_child < len(nodes):
+        for i in range(curr, curr + 2 ** level):
+            if nodes[i]:
+                nodes[i].left = nodes[next_child]
+                nodes[i].right = nodes[next_child + 1]
+            next_child += 2
+        curr = i + 1
+        level += 1
+    return nodes[0]
+
+if __name__== '__main__':
+    solution = Solution()
+
+    # nums = [3,2,3,'null',3,'null',1]
+    nums = [3,4,5,1,3,'null',1]
+    root = convert2TreeNode(nums)
+    # print(root.val)
+
+    ans = solution.rob(root)
+    # print(ans == 7)
+    print(ans == 9)
+
